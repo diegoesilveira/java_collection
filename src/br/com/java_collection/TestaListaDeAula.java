0b346ab3b4d87f3b8c9b8e279430b4420666ad80
diegoesilveira <diegoesilveira@gmail.com>
2019-03-08 23:43:54 -0300

    Ajustes.

--- src/br/com/java_collection/TestaListaDeAula.java
+++ src/br/com/java_collection/TestaListaDeAula.java
@@ -1,8 +1,21 @@
 package br.com.java_collection;
 
+import java.util.ArrayList;
+
 public class TestaListaDeAula {
-    Aula a1 = new Aula("Revisitando arraylist", 21);
-    Aula a2 = new Aula("Lista de objetos", 21);
+
+    public static void main(String[] args) {
+        Aula a1 = new Aula("Revisitando arraylist", 21);
+        Aula a2 = new Aula("Lista de objetos", 35);
+        Aula a3 = new Aula("Lista de objetos", 41);
+
+        ArrayList<Aula> aulas = new ArrayList<>();
+        aulas.add(a1);
+        aulas.add(a2);
+        aulas.add(a3);
+
+        System.out.println(aulas);
+    }
 
 
 }
