e878c8242f2e7db65cc52be8b4b45f68049d42e0
diegoesilveira <diegoesilveira@gmail.com>
2019-03-10 00:47:45 -0300

    Criado a classe aluno, agora vamos usar o Set.

---
+++ src/br/com/java_collection/Aluno.java
@@ -0,0 +1,20 @@
+package br.com.java_collection;
+
+public class Aluno {
+
+    private String nome;
+    private int numeroMatricula;
+
+    public Aluno(String nome, int numeroMatricula) {
+        this.nome = nome;
+        this.numeroMatricula = numeroMatricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getNumeroMatricula() {
+        return numeroMatricula;
+    }
+}
