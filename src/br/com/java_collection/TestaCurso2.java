bf89bdb06671cb1ff048cbb3b5ae5739fa2f53bd
diegoesilveira <diegoesilveira@gmail.com>
2019-03-09 22:14:53 -0300

    Criado a classe TestaCurso2.

---
+++ src/br/com/java_collection/TestaCurso2.java
@@ -0,0 +1,13 @@
+package br.com.java_collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestaCurso2 {
+
+    public static void main(String[] args) {
+        Curso javaColecoes = new Curso("Trabalhando com ArrarList", "Diego Silveira");
+        System.out.println(javaColecoes);
+    }
+
+}
