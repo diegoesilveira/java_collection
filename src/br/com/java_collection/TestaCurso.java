d8ecb107a164f691460f09b697d69555334b64dd
diegoesilveira <diegoesilveira@gmail.com>
2019-03-09 22:14:08 -0300

    Criado uma List de Aula.

--- src/br/com/java_collection/TestaCurso.java
+++ src/br/com/java_collection/TestaCurso.java
@@ -7,10 +7,20 @@ public class TestaCurso {
     public static void main(String[] args) {
         Curso javaColecoes = new Curso("Dominando as collection", "Diego Silveira");
 
-        javaColecoes.adiciona(new Aula("Dominando colecoes0", 12));
+        javaColecoes.adiciona(new Aula("Dominando colecoes", 12));
         javaColecoes.adiciona(new Aula("List ArrayList", 22));
         javaColecoes.adiciona(new Aula("Linkedlist", 32));
         javaColecoes.adiciona(new Aula("Collection", 55));
+
+        List<Aula> aulas = javaColecoes.getAulas();
+        System.out.println(aulas);
+        System.out.println("----------------------------------");
+
+        javaColecoes.adiciona(new Aula("Trabalhando com ArraysList", 40));
+
+
+
+
 
     }
 }
