48ac16e85f91dadb1a97c7b42d2eb4d757d17c04
diegoesilveira <diegoesilveira@gmail.com>
2019-03-10 01:33:31 -0300

    Classe TestaAluno vamos usar o Set.

--- src/br/com/java_collection/TestaAlunos.java
+++ src/br/com/java_collection/TestaAlunos.java
@@ -5,37 +5,54 @@ import java.util.*;
 public class TestaAlunos {
 
     public static void main(String[] args) {
-        Set<String> alunos = new HashSet<>();
+        Curso javaColecoes = new Curso("Curso Collection", "Diego Silveira");
 
-        alunos.add("Joao");
-        alunos.add("Victor");
-        alunos.add("Jessika");
-        alunos.add("Diego");
-        alunos.add("Silveira");
-        alunos.add("Evaldt");
-        alunos.add("Santos");
-        alunos.add("Diego");
+        javaColecoes.adiciona(new Aula("Trabalhando com Set", 20));
+        javaColecoes.adiciona(new Aula("Trabalhando com HashSet", 20));
+        javaColecoes.adiciona(new Aula("Criando uma aula com Set", 20));
+        javaColecoes.adiciona(new Aula("Modelando aula com Set", 20));
+        javaColecoes.adiciona(new Aula("Entendendo o Set", 20));
 
-        System.out.println(alunos.size());
-        System.out.println("-------------------------------------");
-        for (String aluno : alunos) {
-            System.out.println(aluno);
-        }
-        System.out.println("-------------------------------------");
-        System.out.println(alunos.contains("Joao"));
-        System.out.println("-------------------------------------");
+        Aluno a1 = new Aluno("Diego", 23454);
+        Aluno a2 = new Aluno("Jessika", 28125);
+        Aluno a3 = new Aluno("Joao", 48626);
+        Aluno a4 = new Aluno("Victor", 7891);
+        Aluno a5 = new Aluno("Diego Silveira", 234541);
 
-        List<String> aluno = new ArrayList<>(alunos);
-        System.out.println(aluno);
-        Collections.sort(aluno);
-        System.out.println(aluno);
-        System.out.println("-------------------------------------");
+        javaColecoes.matricula(a1);
+        javaColecoes.matricula(a2);
+        javaColecoes.matricula(a3);
+        javaColecoes.matricula(a4);
+        javaColecoes.matricula(a5);
 
-        System.out.println(aluno.indexOf("Diego"));
-        System.out.println(aluno.indexOf("Victor"));
-        System.out.println(aluno.get(5));
-        System.out.println(aluno.remove(4));
-        System.out.println(aluno);
+        System.out.println("Alunos matriculados:");
+        javaColecoes.getAlunos().forEach(a-> {
+            System.out.println(a);
+        });
+
+        System.out.println("--------------------------------------------");
+
+
+//        System.out.println(alunos.size());
+//        System.out.println("-------------------------------------");
+//        for (String aluno : alunos) {
+//            System.out.println(aluno);
+//        }
+//        System.out.println("-------------------------------------");
+//        System.out.println(alunos.contains("Joao"));
+//        System.out.println("-------------------------------------");
+//
+//        List<String> aluno = new ArrayList<>(alunos);
+//        System.out.println(aluno);
+//        Collections.sort(aluno);
+//        System.out.println(aluno);
+//        System.out.println("-------------------------------------");
+//
+//        System.out.println(aluno.indexOf("Diego"));
+//        System.out.println(aluno.indexOf("Victor"));
+//        System.out.println(aluno.get(5));
+//        System.out.println(aluno.remove(4));
+//        System.out.println(aluno);
 
     }
 }
