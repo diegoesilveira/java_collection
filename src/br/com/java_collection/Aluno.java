9a5ec45ce8b9a89c8ad27749600c98135793f7be
diegoesilveira <diegoesilveira@gmail.com>
2019-03-19 06:30:46 -0300

    Implementado o Comparable na classe Aluno, equals e hasCode.

--- src/br/com/java_collection/Aluno.java
+++ src/br/com/java_collection/Aluno.java
@@ -1,6 +1,6 @@
 package br.com.java_collection;
 
-public class Aluno {
+public class Aluno implements Comparable<Aluno>{
 
     private String nome;
     private int numeroMatricula;
@@ -19,10 +19,26 @@ public class Aluno {
     }
 
     @Override
+    public boolean equals(Object obj) {
+        Aluno outro = (Aluno) obj;
+        return this.nome.equals(outro.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.nome.hashCode();
+    }
+
+    @Override
     public String toString() {
         return "Aluno { " +
                 "Nome= '" + nome + '\'' +
                 ", Numero Matricula= " + numeroMatricula +
                 '}';
+    }
+
+    @Override
+    public int compareTo(Aluno outroAluno) {
+        return this.nome.compareTo(outroAluno.nome);
     }
 }
